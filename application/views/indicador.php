12578293a7c741b052d0bb729160550f4aabefd7
yomaco <yomaco2@hotmail.com>
2015-10-13 14:39:27 -0300

    subiendo diseño de localidad y periodo

--- application/views/indicador.php
+++ application/views/indicador.php
@@ -39,11 +39,210 @@
                       </table>
 <!--                  </div> <br>-->
  
-                  <center>
-                      
-                      <button type="button" id="btnListar" class="btn btn-primary btn-lg btn-block">Mostrar Indicadores</button>
-                  </center>
-                  </form>
+                    <center>
+                        <b>Localidad</b>
+                    </center>
+                    <div class="row">
+                        
+                        <div class="col-md-12">
+                            <table class="table table-condensed">
+                                <tr>
+                                    <td>Región/Pronvincia:</td>
+                                    <td>Distrital:</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <select>
+                                            <option value="">Seleccionar</option>
+                                            <option>Región Apurímac</option>                                
+                                            <option>Abancay</option>                                
+                                            <option>Andahuaylas</option>
+                                            <option>Antabamba</option>
+                                            <option>Aymaraes</option>
+                                            <option>Cotabambas</option>
+                                            <option>Chincheros</option>                                                                                                
+                                            <option>Grau</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <select>
+                                            <option></option>
+                                        </select>                                        
+                                    </td>
+                                </tr>
+                            </table>
+                            </div>
+                    </div>
+                    <center>
+                        <b>Periodo</b>
+                    </center>
+
+                    <div class="row">
+                        
+                        <div class="col-md-12">
+                        <table class="table table-condensed">
+                            <tr>
+                                <td>Fecha Inicial:</td>
+                                <td>
+                                    <select>
+                                        <option>01</option>
+                                        <option>02</option>
+                                        <option>03</option>
+                                        <option>04</option>
+                                        <option>05</option>
+                                        <option>06</option>
+                                        <option>07</option>
+                                        <option>08</option>
+                                        <option>09</option>
+                                        <option>10</option>
+                                        <option>11</option>
+                                        <option>12</option>
+                                        <option>13</option>
+                                        <option>14</option>
+                                        <option>15</option>
+                                        <option>16</option>
+                                        <option>17</option>
+                                        <option>18</option>
+                                        <option>19</option>
+                                        <option>20</option>
+                                        <option>21</option>
+                                        <option>22</option>
+                                        <option>23</option>
+                                        <option>24</option>
+                                        <option>25</option>
+                                        <option>26</option>
+                                        <option>27</option>
+                                        <option>28</option>
+                                        <option>29</option>
+                                        <option>30</option>
+                                        <option>31</option>                                        
+                                    </select>
+                                    
+                                </td>
+                                <td>
+                                    <select>
+                                        <option>01</option>
+                                        <option>02</option>
+                                        <option>03</option>
+                                        <option>04</option>
+                                        <option>05</option>
+                                        <option>06</option>
+                                        <option>07</option>
+                                        <option>08</option>
+                                        <option>09</option>
+                                        <option>10</option>
+                                        <option>11</option>
+                                        <option>12</option>                                                                            
+                                    </select>
+                                    
+                                </td>
+                                <td>
+                                    <select>
+                                        <option>2001</option>
+                                        <option>2002</option>
+                                        <option>2003</option>
+                                        <option>2004</option>
+                                        <option>2005</option>
+                                        <option>2006</option>
+                                        <option>2007</option>
+                                        <option>2008</option>
+                                        <option>2009</option>
+                                        <option>2010</option>
+                                        <option>2011</option>
+                                        <option>2012</option>
+                                        <option>2013</option>
+                                        <option>2014</option>
+                                        <option>2015</option>
+                                        <option>2016</option>
+                                        <option>2017</option>                                                                             
+                                    </select>
+                                    
+                                </td>
+                            </tr>
+                            
+                            <tr>
+                                <td>Fecha Final:</td>
+                                <td>
+                                    <select>
+                                        <option>01</option>
+                                        <option>02</option>
+                                        <option>03</option>
+                                        <option>04</option>
+                                        <option>05</option>
+                                        <option>06</option>
+                                        <option>07</option>
+                                        <option>08</option>
+                                        <option>09</option>
+                                        <option>10</option>
+                                        <option>11</option>
+                                        <option>12</option>
+                                        <option>13</option>
+                                        <option>14</option>
+                                        <option>15</option>
+                                        <option>16</option>
+                                        <option>17</option>
+                                        <option>18</option>
+                                        <option>19</option>
+                                        <option>20</option>
+                                        <option>21</option>
+                                        <option>22</option>
+                                        <option>23</option>
+                                        <option>24</option>
+                                        <option>25</option>
+                                        <option>26</option>
+                                        <option>27</option>
+                                        <option>28</option>
+                                        <option>29</option>
+                                        <option>30</option>
+                                        <option>31</option>                                        
+                                    </select>
+                                </td>
+                                <td>
+                                    <select>
+                                        <option>01</option>
+                                        <option>02</option>
+                                        <option>03</option>
+                                        <option>04</option>
+                                        <option>05</option>
+                                        <option>06</option>
+                                        <option>07</option>
+                                        <option>08</option>
+                                        <option>09</option>
+                                        <option>10</option>
+                                        <option>11</option>
+                                        <option>12</option>                                                                            
+                                    </select>
+                                </td>
+                                <td>
+                                    <select>
+                                        <option>2001</option>
+                                        <option>2002</option>
+                                        <option>2003</option>
+                                        <option>2004</option>
+                                        <option>2005</option>
+                                        <option>2006</option>
+                                        <option>2007</option>
+                                        <option>2008</option>
+                                        <option>2009</option>
+                                        <option>2010</option>
+                                        <option>2011</option>
+                                        <option>2012</option>
+                                        <option>2013</option>
+                                        <option>2014</option>
+                                        <option>2015</option>
+                                        <option>2016</option>
+                                        <option>2017</option>                                                                             
+                                    </select>
+                                </td>
+                            </tr>
+                        </table>                        
+                            </div>
+                    </div>
+                    <br>
+                    <center>
+                        <button type="button" id="btnListar" class="btn btn-default">Mostrar Indicadores</button>
+                    </center>
+                  </form> <!--fin form para cargar indicadores-->
                     
             </div>
           </div>
